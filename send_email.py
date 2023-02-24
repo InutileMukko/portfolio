4c7920937590885b2f93c214a8169399a68b4485
InutileMukko <inutilemukko@gmail.com>
2023-02-24 16:39:41 +0100

    Added send email function

---
+++ send_email.py
@@ -0,0 +1,26 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = 'Ivan.Donati71@gmail.com'
+    password = 'drtddrdfjeddzduq'
+
+    receiver = 'Ivan.Donati71@gmail.com'
+    context = ssl.create_default_context()
+
+    # la \ serve per dire che il messaggio non inizia con una nuova riga ma con Subject
+    # per separare il subject dal resto serve una riga vuota dopo
+    # message = '''\
+    # Subject: test email
+    #
+    # text
+    # hi
+    # hello
+    # '''
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
