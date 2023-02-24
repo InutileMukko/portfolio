f88db5f517bc9104e90685251ec6e30e157f30d9
InutileMukko <inutilemukko@gmail.com>
2023-02-24 17:52:52 +0100

    Added environment variable for mail pwd

--- send_email.py
+++ send_email.py
@@ -1,4 +1,4 @@
-import smtplib, ssl
+import smtplib, ssl, os
 
 
 def send_email(message):
@@ -6,7 +6,7 @@ def send_email(message):
     port = 465
 
     username = 'Ivan.Donati71@gmail.com'
-    password = 'drtddrdfjeddzduq'
+    password = os.getenv('PASSWORD')
 
     receiver = 'Ivan.Donati71@gmail.com'
     context = ssl.create_default_context()
